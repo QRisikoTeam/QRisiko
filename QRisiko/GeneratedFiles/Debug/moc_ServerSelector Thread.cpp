bc8e8d773f8e1529f248d96d5feee1dea12505ba
V Ronin <v.ronin@yahoo.it>
2012-04-24 19:47:21 +0200

    Eliminato uso di QHttp in quanto deprecato e sostituito con QNetworkAccessManager nella classe GestoreServers
    Aggiunto menu Regolamento
    Da implementare funzionalità di inserimento manuale dell'IP nella classe ServerSelector e Rimozioni di IP non validi dalla lista dei server

--- QRisiko/GeneratedFiles/Debug/moc_ServerSelector Thread.cpp
+++ QRisiko/GeneratedFiles/Debug/moc_ServerSelector Thread.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'ServerSelector Thread.h'
 **
-** Created: Tue 24. Apr 14:25:58 2012
+** Created: Tue 24. Apr 19:44:27 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
